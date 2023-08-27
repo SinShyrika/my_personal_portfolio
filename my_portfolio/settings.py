a4002f97b77d202fa68adb7ac353456e85849bec
Dima <dmitry.sereda.d@gmail.com>
2023-08-27 19:23:26 +0000

    added a gitignore

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ktndw2o4wioy$)5-p&m_5%@7@y+vy_$r=3azs#&2$m_zgc%y*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sinshyrika1.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
